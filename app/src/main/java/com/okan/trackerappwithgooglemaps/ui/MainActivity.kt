5babe997c4a19e2d5382425b13af909b3e3c2977
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-18 16:27:35 +0300

    Navigation Component

--- app/src/main/java/com/okan/trackerappwithgooglemaps/ui/MainActivity.kt
+++ app/src/main/java/com/okan/trackerappwithgooglemaps/ui/MainActivity.kt
@@ -2,22 +2,31 @@ package com.okan.trackerappwithgooglemaps.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.okan.trackerappwithgooglemaps.R
-import com.okan.trackerappwithgooglemaps.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RunDao: ${runDAO.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 
 }
--- app/src/main/java/com/okan/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/okan/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
@@ -1,10 +1,14 @@
 package com.okan.trackerappwithgooglemaps.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.okan.trackerappwithgooglemaps.R
 import com.okan.trackerappwithgooglemaps.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 /** While injecting viewModels in the fragment we do not have to write inject
  *  We use private val viewModel: MainViewModel by viewModels() this line of code
@@ -15,5 +19,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 
 }
