27c0a202039ec885cfcfd0c28abef7a0efb28537
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-19 13:31:03 +0300

    Handling Permissions With Easy Permission Library

--- app/src/main/java/com/okan/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/okan/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.okan.trackerappwithgooglemaps.ui.fragments
 
+import android.Manifest.*
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,8 +9,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.okan.trackerappwithgooglemaps.R
 import com.okan.trackerappwithgooglemaps.ui.viewmodels.MainViewModel
+import com.okan.trackerappwithgooglemaps.utils.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.okan.trackerappwithgooglemaps.utils.TrackingUtility
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 /** While injecting viewModels in the fragment we do not have to write inject
  *  We use private val viewModel: MainViewModel by viewModels() this line of code
@@ -16,15 +22,65 @@ import kotlinx.android.synthetic.main.fragment_run.*
  */
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requestPermissions()
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermissions() {
+        if (TrackingUtility.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                permission.ACCESS_COARSE_LOCATION,
+                permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                permission.ACCESS_COARSE_LOCATION,
+                permission.ACCESS_FINE_LOCATION,
+                permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults,
+            this
+        )
     }
 
 }
---
+++ app/src/main/java/com/okan/trackerappwithgooglemaps/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.okan.trackerappwithgooglemaps.utils
+
+object Constants {
+
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
+}
